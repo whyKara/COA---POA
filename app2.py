c05534615097d1780217a7268444734dcfabd740
Yash Karapurkar <75109496+whyKara@users.noreply.github.com>
2023-05-30 12:17:41 +0530

    2 tests

--- app2.py
+++ app2.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+from openpyxl import load_workbook
 import sqlite3
 
 # Connect to the database
@@ -31,37 +31,56 @@ def insert_data_to_db(data):
 # Read specific cells from the Excel file
 def read_excel_file(file, sheet_name):
     try:
-        # Read the selected sheet into a DataFrame
-        df = pd.read_excel(file, sheet_name=sheet_name)
+        # Load the Excel workbook
+        workbook = load_workbook(file, read_only=True)
 
-        # Extract the required data from the DataFrame
+        # Select the sheet by name
+        sheet = workbook[sheet_name]
+
+        # Extract the required data from the sheet
+        class_id = sheet['A8'].value
+        subject_id = sheet['A10'].value
+
+        # Check if the extracted values are None or empty strings
+        if not class_id or not subject_id:
+            st.warning("Class ID or Subject ID is not valid.")
+            return
+
         data = {
-            'Class ID': df.iloc[7, 0],
-            'Test ID': df.iloc[17, 2],
-            'Subject ID': df.iloc[9, 0]
+            'Class ID': class_id,
+            'Subject ID': subject_id
         }
 
-        # Retrieve the questions, marks, and CO attainment values
-        for col in range(3, 9):
-            question_id = df.iloc[18, col]
-            question_marks = df.iloc[19, col]
-            co_attainment = df.iloc[20, col]
+        # Get the test IDs and their respective column indices
+        test_ids = [sheet.cell(row=18, column=3).value, sheet.cell(row=18, column=12).value]
+        test_columns = [3, 12]
 
-            # Check if the question ID is not empty
-            if pd.notna(question_id):
-                # Insert the data into the database
-                data['Question ID'] = question_id
-                data['Question Marks'] = question_marks
-                data['CO Attainment'] = co_attainment
+        # Iterate over the tests and retrieve the questions, marks, and CO attainment values
+        for test_id, col in zip(test_ids, test_columns):
+            data['Test ID'] = test_id
 
-                insert_data_to_db(data)
-            else:
-                # Break the loop if an empty cell is encountered
-                break
+            # Retrieve the questions, marks, and CO attainment values for the current test
+            for row in range(19, 21):
+                for col_offset in range(0, 4):
+                    cell = sheet.cell(row=row, column=col + col_offset)
+                    question_id = cell.value
+                    question_marks = sheet.cell(row=22, column=col + col_offset).value
+                    co_attainment = sheet.cell(row=23, column=col + col_offset).value
+
+                    # Check if the question ID is not empty
+                    if question_id:
+                        # Insert the data into the database
+                        data['Question ID'] = question_id
+                        data['Question Marks'] = question_marks
+                        data['CO Attainment'] = co_attainment
+
+                        insert_data_to_db(data)
+                    else:
+                        # Break the loop if an empty cell is encountered
+                        break
 
     except Exception as e:
         st.error(f"Error occurred: {str(e)}")
-
 
 # Streamlit app
 def main():
@@ -75,11 +94,12 @@ def main():
         st.header("Upload Excel File")
         file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
 
-    if file is not None:
-        # Read the selected sheet from the uploaded Excel file
-        sheet_names = pd.ExcelFile(file).sheet_names
-        selected_sheet = st.selectbox("Select Sheet", sheet_names)
-        read_excel_file(file, selected_sheet)
+        if file is not None:
+            # Read the selected sheet from the uploaded Excel file
+            workbook = load_workbook(file, read_only=True)
+            sheet_names = workbook.sheetnames
+            selected_sheet = st.selectbox("Select Sheet", sheet_names)
+            read_excel_file(file, selected_sheet)
 
 # Run the app
 if __name__ == "__main__":
